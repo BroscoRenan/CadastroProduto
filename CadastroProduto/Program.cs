f854e4cdbf4bd348f587e3886a12f3cacbe97365
BroscoRenan <renan.rocha905@gmail.com>
2023-08-10 19:00:33 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroProduto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroProduto{
+     class Program {  
+        static void Main(string[] args) {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("QuaNtidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);            
+            Console.Write("Dados atualizados " +  p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoveProdutos(qte);            
+            Console.WriteLine("Dados atualizaos: " + p);
+
+            Console.ReadKey();
+        }
+    }
+}
